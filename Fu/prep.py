c7fc713d7e575511fa73d9eeb36661f6564d7d06
michael ho <michaelho0307@gmail.com>
2024-11-09 05:42:20 +0000

    add comment all print in Fu dictrectory

--- Fu/prep.py
+++ Fu/prep.py
@@ -6,10 +6,10 @@ def preprocess(text):
 
     # 抽取命名實體
     entities = [ent.text for ent in doc.ents]
-    for token in doc:
-        print(token.text, token.pos_)
+    # for token in doc:
+    #     print(token.text, token.pos_)
     selected_words = [token.text for token in doc if token.pos_ not in ("ADP", "PART", "PROPN") and token.text not in ["多少","是","為"]]
     new_sentence = "".join(selected_words)
-    print(new_sentence)
+    # print(new_sentence)
     return new_sentence
 
